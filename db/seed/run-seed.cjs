8d6df30172c39739c4f495a00454aced0c9f7a27
code-emad <emadul.islam@hotmail.co.uk>
2023-08-13 15:42:01 +0100

    tidy of runseed files

--- db/seed/run-seed.cjs
+++ db/seed/run-seed.cjs
@@ -4,7 +4,8 @@ var devData = require("../data/data-dev/index.ts");
 var connection_cjs_1 = require("../connection.cjs");
 var seed_cjs_1 = require("./seed.cjs");
 var runSeed = function () {
-    return (0, seed_cjs_1.seed)(devData).then(function () { return connection_cjs_1.db.end(); });
+  return (0, seed_cjs_1.seed)(devData).then(function () {
+    return connection_cjs_1.db.end();
+  });
 };
 runSeed();
-console.log(seed_cjs_1.seed);
--- db/seed/run-seed.cts
+++ db/seed/run-seed.cts
@@ -1,10 +1,9 @@
-const devData = require(`../data/data-dev/index.ts`)
-import {db} from '../connection.cjs'
-import {seed} from './seed.cjs'
+const devData = require(`../data/data-dev/index.ts`);
+import { db } from "../connection.cjs";
+import { seed } from "./seed.cjs";
 
 const runSeed = () => {
   return seed(devData).then(() => db.end());
 };
 
 runSeed();
-console.log(seed)
