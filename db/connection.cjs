379c988c4107e18020fdd4c36b027fc8ea53f7cc
code-emad <emadul.islam@hotmail.co.uk>
2023-08-13 15:40:14 +0100

    tidying up

--- db/connection.cjs
+++ db/connection.cjs
@@ -1,19 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
-var Pool = require('pg').Pool;
-var ENV = process.env.NODE_ENV || 'development';
-require('dotenv').config({
-    path: "".concat(__dirname, "/../.env.").concat(ENV),
+var Pool = require("pg").Pool;
+var ENV = process.env.NODE_ENV || "development";
+require("dotenv").config({
+  path: "".concat(__dirname, "/../.env.").concat(ENV),
 });
-var config = ENV === 'production'
+var config =
+  ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
         max: 2,
-    }
+      }
     : {};
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error('PGDATABASE or DATABASE_URL not set');
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 exports.db = new Pool(config); // Create a new Pool instance
-// module.exports = db; // Export the db object
