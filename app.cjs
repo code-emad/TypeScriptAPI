6cd31691ca2afee5eab6988914fa26efad1f61a6
code-emad <emadul.islam@hotmail.co.uk>
2023-08-30 18:11:06 +0100

    rerunning tsc app.cts

--- app.cjs
+++ app.cjs
@@ -1,28 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var express = require('express');
+var express = require("express");
 var app = express();
-var cors = require('cors');
+var cors = require("cors");
 var connection_cjs_1 = require("./db/connection.cjs");
 app.use(cors());
 app.use(express.json());
-app.get('/api/names', function (request, response, next) {
-    var sqlString = 'SELECT * FROM names;';
+app.get("/api/names", 
+//controller
+function (request, response, next) {
+    //model
+    var sqlString = "SELECT * FROM names;";
     connection_cjs_1.db.query(sqlString)
+        //controller
         .then(function (names) {
         console.log(names);
         response.status(200).send({ hello: "world" });
     });
-    // () => {
-    //     const sqlString = 'SELECT * FROM names;'
-    // return db.query(sqlString).then((names) => {
-    //     console.log(names)
-    //     response.status(200).send({"hello": "world"})
-    // })
-    // }
 });
 // listen file
 //const app = require('./app') will need to be included once moved to separate file
 var _a = process.env.PORT, PORT = _a === void 0 ? 9090 : _a;
 app.listen(PORT, function () { return console.log("Listening on port ".concat(PORT, "...")); });
-console.log(express);
