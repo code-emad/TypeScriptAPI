f37e372748395f49846d91508fdd86a67bca6257
code-emad <emadul.islam@hotmail.co.uk>
2023-08-30 18:02:28 +0100

    updating test data import

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../app.cjs')
 const db = require ('../db/connection.cjs')
 const seed = require('../db/seed/seed.cjs')
-const testData = require('../db/data/data-test/index.cjs')
+const testData = require('../db/data/data-test/index.ts')
 
 beforeEach(() => seed(testData));
 
