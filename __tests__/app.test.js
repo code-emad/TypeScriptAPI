73f2590583b5b6a134055f75e32f8d64c487c97f
code-emad <emadul.islam@hotmail.co.uk>
2023-09-05 17:45:19 +0100

    change to test if gh working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,6 +10,9 @@ console.log({app: app}, {db: db}, seed, testData)
 
 // afterAll(() => {db.end()})
 
+
+//test
+
 describe('GET/api/names', () => {
     test('should return a status code of 200', () => {
         return request(app).get('/api/names').expect(200)
