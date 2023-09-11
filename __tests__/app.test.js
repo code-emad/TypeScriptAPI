b99778210ac05afaffbdbedb5c5d53e9bdfe27d5
code-emad <emadul.islam@hotmail.co.uk>
2023-09-11 20:28:19 +0100

    test doc formatting

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,20 +1,19 @@
-const request = require('supertest')
-const app = require('../app.cjs')
-const db = require ('../db/connection.cjs')
-const seed = require('../db/seed/seed.cjs')
-const testData = require('../db/data/data-test/index.ts')
+const request = require("supertest");
+const app = require("../app.cjs");
+const db = require("../db/connection.cjs");
+const seed = require("../db/seed/seed.cjs");
+const testData = require("../db/data/data-test/index.ts");
 
-console.log({app: app}, {db: db}, seed, testData)
+console.log({ app: app }, { db: db }, seed, testData);
 
 // beforeEach(() => seed(testData));
 
 // afterAll(() => {db.end()})
 
-
 //test
 
-describe('GET/api/names', () => {
-    test('should return a status code of 200', () => {
-        return request(app).get('/api/names').expect(200)
-    })
+describe("GET/api/names", () => {
+  test("should return a status code of 200", () => {
+    return request(app).get("/api/names").expect(200);
+  });
 });
